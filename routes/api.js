11dc895d4abc459ce365d09127e5b768e9a285d9
arielzebaze-a11 <ariel.zebaze@facsciences-uy1.cm>
2026-05-04 10:33:08 +0100

    fix: updated login labels and logic

--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const authCtrl = require('../controllers/authController');
 const transacCtrl = require('../controllers/transactionController');
 const adminCtrl = require('../controllers/adminController');
-const transactionController = require('../controllers/transactionController');
 
 // --- AUTHENTIFICATION ---
 router.post('/auth/register', authCtrl.register);
 router.post('/auth/login', authCtrl.login);
-router.post('/account/balance', transacCtrl.getBalance);
+router.put('/user/update', authCtrl.updateProfile); // Ajouté pour Swagger
 
 // --- UTILISATEUR (CLIENT) ---
-router.post('/account/balance', accountController.getBalance);
+// Utilise transacCtrl au lieu de accountController
+router.post('/account/balance', transacCtrl.getBalance); 
 router.get('/transactions/history/:userId', transacCtrl.getHistory);
 router.post('/transactions/transfer', transacCtrl.transfer);
 router.post('/transactions/deposit', transacCtrl.deposit);

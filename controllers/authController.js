62ba4377fd903e824b725557ceb01e8f78d86f73
arielzebaze-a11 <ariel.zebaze@facsciences-uy1.cm>
2026-06-17 17:04:07 +0100

    nouvelle version

--- controllers/authController.js
+++ controllers/authController.js
@@ -289,16 +289,16 @@ exports.login = async (req, res) => {
         );
 
         res.json({
-        message: "✅ Connexion réussie",
-        token,
-        compte: {
-            id: compte.id,
-            banque: banque.nom,
-            solde: compte.solde,
-            user: utilisateur.nom,
-            telephone: utilisateur.telephone,
-            role: utilisateur.role
-        }
+            message: "✅ Connexion réussie",
+            token,
+            compte: {
+                id: account.id,
+                banque: bank.nom,
+                solde: account.solde,
+                user: user.nom,
+                telephone: user.telephone,
+                role: user.role
+            }
         });
 
     } catch (error) {
